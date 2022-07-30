f970555db91505f3d8e4804e262f2eb9aa80ca11
João Vitor Lima <jvolima2004@gmail.com>
2022-07-29 21:44:52 -0300

    Salvando solicitações no firestore

--- src/screens/Register.tsx
+++ src/screens/Register.tsx
@@ -1,9 +1,45 @@
 import { VStack } from 'native-base';
+import firestore from "@react-native-firebase/firestore";
+import { useState } from 'react';
+import { Alert } from 'react-native';
 import { Button } from '../components/Button';
 import { Header } from '../components/Header';
 import { Input } from '../components/Input';
+import { useNavigation } from '@react-navigation/native';
 
 export function Register() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [patrimony, setPatrimony] = useState("");
+  const [description, setDescription] = useState("");
+
+  const navigation = useNavigation();
+
+  function handleNewOrderRegister() {
+    if(!patrimony || !description) {
+      return Alert.alert("Registrar", "Preencha todos os campos.");
+    }
+
+    setIsLoading(true);
+
+    firestore()
+    .collection("orders")
+    .add({
+      patrimony,
+      description,
+      status: "open",
+      created_at: firestore.FieldValue.serverTimestamp()
+    })
+    .then(() =>{
+      Alert.alert("Solicitação", "Solicitação registrada com sucesso.");
+      navigation.goBack();
+    })
+    .catch((error) => {
+      console.log(error);
+      setIsLoading(false);
+      return Alert.alert("Solicitação", "Não foi possível registrar o pedido.");
+    })
+  }
+
   return (
     <VStack flex={1} p={6} bg="gray.600">
       <Header title="Nova solicitação" />
@@ -11,6 +47,7 @@ export function Register() {
       <Input
         placeholder="Número do pratimônio"
         mt={4}
+        onChangeText={setPatrimony}
       />
 
       <Input
@@ -19,11 +56,14 @@ export function Register() {
         flex={1}
         multiline
         textAlignVertical="top"
+        onChangeText={setDescription}
       />
 
       <Button 
         title="Cadastrar"
         mt={5}
+        isLoading={isLoading}
+        onPress={handleNewOrderRegister}
       />
     </VStack>
   );
